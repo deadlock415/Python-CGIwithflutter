3a2b8876993d9bc6c07e34f42969c132ccd85b79
deadlock415 <arpitchalakh@gmail.com>
2021-08-22 20:25:39 +0530

    upload app

---
+++ android/app/src/main/kotlin/com/example/app25june/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app25june
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  web() async {
+    var url = Uri.http("192.168.1.2", "/cgi-bin/cmd.py", {"x": cmd, "y": cn});
+    var response = await http.get(url);
+    print(response.body);
+    // print(cmd);
+  }
+
+  late String cmd;
+  late String cn;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        
+        appBar: AppBar(
+          title: Text("Python-CGI With Flutter"),
+        ),
+        body: Column(
+          children: [
+            Container(
+            margin: const EdgeInsets.all(10.0),
+            child: Text("Enter Your Command : ",
+            style: TextStyle(fontSize: 30.0),
+
+            ),
+            ),
+            TextField(
+              onChanged: (value) {
+                cmd = value;
+              },
+              autocorrect: false,
+              textInputAction: TextInputAction.send,
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.airplay),
+                  hintText: "Enter Docker Image"),
+            ),
+            TextField(
+              onChanged: (value) {
+                cn = value;
+              },
+              autocorrect: false,
+              textInputAction: TextInputAction.send,
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.add_to_queue),
+                  hintText: "Enter Docker Name"),
+            ),
+            OutlinedButton(
+              onPressed: web,
+              child:  Text('RUN',
+              style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+           
+          ],
+        ),
+      ),
+    );
+  }
+}
